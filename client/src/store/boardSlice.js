546f3a998743a50a0d419e250832cc63965151b4
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-09-10 15:29:52 -0300

    feat(store): add vote update reducer to boardSlice

--- client/src/store/boardSlice.js
+++ client/src/store/boardSlice.js
@@ -17,6 +17,29 @@ const boardSlice = createSlice({
     addOption: (state, action) => {
       state.options.unshift(action.payload);
     },
+    applyVoteUpdate: (state, action) => {
+      const {
+        optionId,
+        likesCount,
+        dislikesCount,
+        score,
+        leadingOptionIds,
+        vote,
+      } = action.payload;
+      const option = state.options.find((o) => o.id === optionId);
+      if (!option) return;
+
+      if (likesCount !== undefined) option.likesCount = likesCount;
+      if (dislikesCount !== undefined) option.dislikesCount = dislikesCount;
+      if (score !== undefined) option.score = score;
+      if (vote !== undefined) option.vote = vote;
+      if (Array.isArray(leadingOptionIds)) {
+        const leadingSet = new Set(leadingOptionIds);
+        for (const opt of state.options) {
+          opt.isLeading = leadingSet.has(opt.id);
+        }
+      }
+    },
     setStatus: (state, action) => {
       state.status = action.payload;
     },
@@ -28,6 +51,12 @@ const boardSlice = createSlice({
   },
 });
 
-export const { setBoard, setOptions, addOption, setStatus, resetBoard } =
-  boardSlice.actions;
+export const {
+  setBoard,
+  setOptions,
+  addOption,
+  applyVoteUpdate,
+  setStatus,
+  resetBoard,
+} = boardSlice.actions;
 export default boardSlice.reducer;
